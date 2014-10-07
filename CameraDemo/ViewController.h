2795e23aff5949eea44cbbbc7028a7b539f97caa
yw <yaowei@camera360.com>
2014-10-07 21:57:59 +0800

    Initial commit

---
+++ CameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by weirdln on 14-10-7.
+//  Copyright (c) 2014年 weirdln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
