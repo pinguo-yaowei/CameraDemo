e5456871ce50a49ec84c22fdfef28262eb907d21
yw <yaowei@camera360.com>
2014-10-15 13:40:42 +0800

    import pods!

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// iOS-WebP
+#define COCOAPODS_POD_AVAILABLE_iOS_WebP
+#define COCOAPODS_VERSION_MAJOR_iOS_WebP 0
+#define COCOAPODS_VERSION_MINOR_iOS_WebP 3
+#define COCOAPODS_VERSION_PATCH_iOS_WebP 0
+
